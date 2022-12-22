291509b602b2e783e4def68670fbfe337100a7d6
Lukas Cunningham <97056435+Luke-Cunningham@users.noreply.github.com>
2022-12-22 13:11:16 -0800

    binary search

--- main.py
+++ main.py
@@ -40,6 +40,30 @@ def search(nums, target):
     return -1
 
 
+def searchMatrix(matrix, target):
+    """
+    :type matrix: List[List[int]]
+    :type target: int
+    :rtype: bool
+    """
+
+    r, c = len(matrix), len(matrix[0])
+    start, end = 0, r * c - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        num = matrix[mid / c][mid % c]
+
+        if num == target:
+            return True
+        elif num < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return False
+
+
 if __name__ == '__main__':
     case1 = [1,2,3,1]
     containsDuplicate(case1)
@@ -49,4 +73,7 @@ if __name__ == '__main__':
 
     case1 = [-1, 0, 3, 5, 9, 12]
     search(case1, 5)
+
+    m1 = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+    searchMatrix(m1, 16)
 
